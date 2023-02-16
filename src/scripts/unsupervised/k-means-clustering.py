643b992a8cbd6367af857c27192dcb744b75dbdf
Manuel Schmid <manuel.schmid@liip.ch>
2023-02-16 16:30:14 +0100

    fix duplicate variable name

--- src/scripts/unsupervised/k-means-clustering.py
+++ src/scripts/unsupervised/k-means-clustering.py
@@ -17,8 +17,8 @@ samples, features = data.shape
 
 
 # Model Training
-def bench_k_means(estimator, name, data):  # function from sklearn for formatted output
-    estimator.fit(data)
+def bench_k_means(estimator, name, _data):  # function from sklearn for formatted output
+    estimator.fit(_data)
     print('%-9s\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
           % (name, estimator.inertia_,
              metrics.homogeneity_score(y, estimator.labels_),
@@ -26,14 +26,9 @@ def bench_k_means(estimator, name, data):  # function from sklearn for formatted
              metrics.v_measure_score(y, estimator.labels_),
              metrics.adjusted_rand_score(y, estimator.labels_),
              metrics.adjusted_mutual_info_score(y,  estimator.labels_),
-             metrics.silhouette_score(data, estimator.labels_,
+             metrics.silhouette_score(_data, estimator.labels_,
                                       metric='euclidean')))
 
 
 clf_model = KMeans(n_clusters=k, init="random", n_init=10)
 bench_k_means(clf_model, "1", data)
-
-
-
-
-
