9a497f13dfc6afc6916eebcf02bc56fb2fdfe02f
Manuel Schmid <manuel.schmid@liip.ch>
2023-02-16 13:47:14 +0100

    implement K-means clustering algorithm for handwritten digit recognition

---
+++ src/scripts/unsupervised/k-means-clustering.py
@@ -0,0 +1,39 @@
+import sklearn
+import numpy as np
+from sklearn.preprocessing import scale
+from sklearn.datasets import load_digits
+from sklearn.cluster import KMeans
+from sklearn import metrics
+
+
+# Data Preparation# Data Preparation
+digits = load_digits()  # load handwritten digits dataset
+data = scale(digits.data)  # scale numbers down to range between -1 & 1
+y = digits.target
+
+# k = len(np.unique(y))
+k = 10  # amount of centroids
+samples, features = data.shape
+
+
+# Model Training
+def bench_k_means(estimator, name, data):  # function from sklearn for formatted output
+    estimator.fit(data)
+    print('%-9s\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
+          % (name, estimator.inertia_,
+             metrics.homogeneity_score(y, estimator.labels_),
+             metrics.completeness_score(y, estimator.labels_),
+             metrics.v_measure_score(y, estimator.labels_),
+             metrics.adjusted_rand_score(y, estimator.labels_),
+             metrics.adjusted_mutual_info_score(y,  estimator.labels_),
+             metrics.silhouette_score(data, estimator.labels_,
+                                      metric='euclidean')))
+
+
+clf_model = KMeans(n_clusters=k, init="random", n_init=10)
+bench_k_means(clf_model, "1", data)
+
+
+
+
+
