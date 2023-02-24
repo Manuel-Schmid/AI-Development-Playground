eba567af6b3fcf5f70ced1d37e9a555eca095511
Manuel Schmid <manuel.schmid@liip.ch>
2023-02-24 16:02:01 +0100

    create and train model

--- src/scripts/neural_networks/yt_title_predictor.py
+++ src/scripts/neural_networks/yt_title_predictor.py
@@ -98,3 +98,28 @@ def generate_padded_sequences(input_sequences):
     return predictors, label, max_sequence_len
 
 
+predictors, label, max_sequence_len = generate_padded_sequences(inp_sequences)
+
+
+def create_model(max_sequence_len, total_words):
+    input_len = max_sequence_len - 1
+    model = Sequential()
+
+    # Add Input Embedding Layer
+    model.add(Embedding(total_words, 10, input_length=input_len))
+
+    # Add Hidden Layer 1 - LSTM Layer
+    model.add(LSTM(100))
+    model.add(Dropout(0.1))
+
+    # Add Output Layer
+    model.add(Dense(total_words, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+    return model
+
+
+model = create_model(max_sequence_len, total_words)
+model.fit(predictors, label, epochs=20, verbose=5)
+
+
