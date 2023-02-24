6d0738d035dfc0276a235694c7816deef09c3296
Manuel Schmid <manuel.schmid@liip.ch>
2023-02-24 16:00:47 +0100

    add tokenizing and sequence padding

--- src/scripts/neural_networks/yt_title_predictor.py
+++ src/scripts/neural_networks/yt_title_predictor.py
@@ -67,3 +67,34 @@ def clean_text(text):
 
 corpus = [clean_text(e) for e in entertainment]
 
+
+tokenizer = Tokenizer()
+
+
+def get_sequence_of_tokens(corpus):
+    # get tokens
+    tokenizer.fit_on_texts(corpus)
+    total_words = len(tokenizer.word_index) + 1
+
+    # convert to sequence of tokens
+    input_sequences = []
+    for line in corpus:
+        token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[:i + 1]
+    input_sequences.append(n_gram_sequence)
+
+    return input_sequences, total_words
+
+
+inp_sequences, total_words = get_sequence_of_tokens(corpus)
+
+
+def generate_padded_sequences(input_sequences):
+    max_sequence_len = max([len(x) for x in input_sequences])
+    input_sequences = np.array(keras.preprocessing.sequence.pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+    predictors, label = input_sequences[:, :-1], input_sequences[:, -1]
+    label = ku.to_categorical(label, num_classes=total_words)
+    return predictors, label, max_sequence_len
+
+
