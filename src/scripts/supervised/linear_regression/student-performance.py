383cb20045dbae1fc600c2e4d64e834988cdb454
Manuel Schmid <manuel.schmid@liip.ch>
2023-04-20 15:47:02 +0200

    extend model training (+ add better console output + optional save)

--- src/scripts/supervised/linear_regression/student-performance.py
+++ src/scripts/supervised/linear_regression/student-performance.py
@@ -5,7 +5,7 @@ from sklearn import linear_model
 import matplotlib.pyplot as pyplot
 import pickle
 from matplotlib import style
-
+import click
 
 data = pd.read_csv("../../../data/student-mat.csv", sep=";")
 data = data[data.G3 != 0]
@@ -28,20 +28,29 @@ x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y
 
 
 # Training
-# best = 0
-# for _ in range(10000):  # trains 30 models and overwrites model each time if performance is better than previous one
-#     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
-#
-#     linear = linear_model.LinearRegression()
-#
-#     linear.fit(x_train, y_train)  # find best fit line using linear regression
-#     acc = linear.score(x_test, y_test)  # check model accuracy
-#
-#     if acc > best:
-#         print(acc)
-#         best = acc
-#         with open("../models/studentmodel.pickle", "wb") as f:  # save trained model in pickle file
-#             pickle.dump(linear, f)
+best = 0
+iterations = 10000
+best_model = None
+for i in range(iterations):  # trains 10k models and overwrites model each time performance is better than previous one
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train, y_train)  # find best fit line using linear regression
+    acc = linear.score(x_test, y_test)  # check model accuracy
+
+    if i % 1000 == 0:
+        print(f'{i}/{iterations}')
+
+    if acc > best:
+        print(i, ":", acc)
+        best = acc
+        best_model = linear
+
+    if i == iterations - 1:
+        if click.confirm(f'Do you want to save the model? (Accuracy: {best})', default=True):
+            with open("../../../models/studentmodel.pickle", "wb") as f:  # save trained model in pickle file
+                pickle.dump(best_model, f)
 
 
 # Using the Model
