67b1e617b309581289eb7781b23c93013c88e76b
Manuel Schmid <manuel.schmid@liip.ch>
2023-05-12 16:56:12 +0200

    implement init, reset & place food function

--- src/snake/game.py
+++ src/snake/game.py
@@ -2,6 +2,7 @@ import pygame
 import numpy as np
 from enum import Enum
 from collections import namedtuple
+import random
 
 pygame.init()
 
@@ -34,4 +35,27 @@ class SnakeGame:
     food = None
     frame_iteration = 0
 
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        # init game state
+        self.direction = Direction.RIGHT
+
+        head = Point(w / 2, h / 2)
+        self.snake = [head,
+                      Point(head.x - BLOCK_SIZE, head.y),
+                      Point(head.x - (2 * BLOCK_SIZE), head.y)]
+
+        score = 0
+        self.food = None
+        self.place_food()
+        self.frame_iteration = 0
+
+    def place_food(self):
+        x = random.randint(0, (w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        y = random.randint(0, (h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        self.food = Point(x, y)
+        if self.food in self.snake:
+            self.place_food()
 
