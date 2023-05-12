88dfd875e8364e357a453c7d14e2c67159bc5db8
Manuel Schmid <manuel.schmid@liip.ch>
2023-05-12 16:52:09 +0200

    implement variables (head, snake, score, etc.)

--- src/snake/game.py
+++ src/snake/game.py
@@ -1,8 +1,10 @@
 import pygame
+import numpy as np
 from enum import Enum
 from collections import namedtuple
 
 pygame.init()
+
 
 class Direction(Enum):
     UP = 1
@@ -12,5 +14,24 @@ class Direction(Enum):
 
 
 Point = namedtuple('Point', 'x, y')
+
+BLOCK_SIZE = 20
+SPEED = 40
+
+
+class SnakeGame:
+    w = 640
+    h = 480
+    display = pygame.display.set_mode((w, h))
+    pygame.display.set_caption('Snake')
+    clock = pygame.time.Clock()
+    direction = Direction.RIGHT
+
+    head = Point(w / 2, h / 2)
+    snake = [head, Point(head.x - BLOCK_SIZE, head.y), Point(head.x - (2 * BLOCK_SIZE), head.y)]
+
+    score = 0
+    food = None
+    frame_iteration = 0
 
 
