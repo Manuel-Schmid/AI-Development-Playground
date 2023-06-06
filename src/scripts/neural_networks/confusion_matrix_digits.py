f2c937c38ff2efd9ef6e2d83808f6dafaf7b2080
Manuel Schmid <manuel.schmid@liip.ch>
2023-06-06 14:28:39 +0200

    adjust confusion matrix variables & constant values

--- src/scripts/neural_networks/confusion_matrix_digits.py
+++ src/scripts/neural_networks/confusion_matrix_digits.py
@@ -8,21 +8,20 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 
 # Load breast cancer dataset
 X, y = load_digits(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 # Train model
-clf = RandomForestClassifier(random_state=23)
+clf = RandomForestClassifier(random_state=25)
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
 
 cm = confusion_matrix(y_test, y_pred)
 
-sns.heatmap(cm,
-            annot=True,
-            fmt='g')
-plt.ylabel('Prediction', fontsize=13)
-plt.xlabel('Actual', fontsize=13)
+#  print plot with confusion matrix heatmap
+sns.heatmap(cm, annot=True, fmt='g')
+plt.ylabel('Prediction', fontsize=14)
+plt.xlabel('Actual', fontsize=14)
 plt.title('Confusion Matrix', fontsize=17)
 plt.show()
 
