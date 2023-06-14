9c0973291ffb140e97d033a687e6f64f6879a7f7
Manuel Schmid <manuel.schmid@liip.ch>
2023-06-14 17:06:23 +0200

    multiple linear regression

--- src/scripts/supervised/linear_regression/multiple_lin_regr.py
+++ src/scripts/supervised/linear_regression/multiple_lin_regr.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib as mpl
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+
 
 def generate_dataset(n):
     x = []
@@ -10,7 +10,7 @@ def generate_dataset(n):
     random_x2 = np.random.rand()
     for i in range(n):
         x1 = i
-        x2 = i/2 + np.random.rand()*n
+        x2 = i / 2 + np.random.rand() * n
         x.append([1, x1, x2])
         y.append(random_x1 * x1 + random_x2 * x2 + 1)
     return np.array(x), np.array(y)
@@ -20,9 +20,9 @@ x, y = generate_dataset(200)
 mpl.rcParams['legend.fontsize'] = 12
 
 fig = plt.figure()
-ax = fig.add_subplot(projection ='3d')
+ax = fig.add_subplot(projection='3d')
 
-ax.scatter(x[:, 1], x[:, 2], y, label ='y', s = 5)
+ax.scatter(x[:, 1], x[:, 2], y, label='y', s=5)
 ax.legend()
 ax.view_init(45, 0)
 
